89efe1692549266aa758ca8a8eb1ba4661bbcf29
DenisUdot <denisudot@gmail.com>
2018-09-24 23:15:51 +0300

    get weather for curent location

--- app/src/main/java/com/denisudotgmail/localweather/GeoLocation.java
+++ app/src/main/java/com/denisudotgmail/localweather/GeoLocation.java
@@ -12,6 +12,7 @@ package com.denisudotgmail.localweather;
  import android.location.LocationManager;
  import android.net.Uri;
  import android.os.Bundle;
+ import android.os.Handler;
  import android.os.Looper;
  import android.provider.Settings;
  import android.support.annotation.NonNull;
@@ -44,6 +45,8 @@ package com.denisudotgmail.localweather;
  import com.karumi.dexter.listener.PermissionRequest;
  import com.karumi.dexter.listener.single.PermissionListener;
 
+ import org.json.JSONObject;
+
  import java.io.IOException;
  import java.text.DateFormat;
  import java.util.Date;
@@ -52,7 +55,10 @@ package com.denisudotgmail.localweather;
 
 
 public class GeoLocation extends Activity {
-
+    Handler handler;
+    public GeoLocation(){
+        handler = new Handler();
+    }
     private static final String TAG = MainActivity.class.getSimpleName();
 
     TextView txtLocationResult;
@@ -61,6 +67,13 @@ public class GeoLocation extends Activity {
     Button btnStartUpdates;
     Button btnStopUpdates;
     Button btnLastLocation;
+
+    //weather
+    TextView cityField;
+    TextView updatedField;
+    TextView detailsField;
+    TextView currentTemperatureField;
+    TextView weatherIcon;
 
     // location last updated time
     private String mLastUpdateTime;
@@ -98,6 +111,13 @@ public class GeoLocation extends Activity {
         btnStopUpdates.setOnClickListener(new stopLocationButtonClickListener());
         btnLastLocation = findViewById(R.id.btn_get_last_location);
         btnLastLocation.setOnClickListener(new showLastKnownLocationListener());
+        // init weather textView
+        cityField = (TextView)findViewById(R.id.city_field);
+        updatedField = (TextView)findViewById(R.id.updated_field);
+        detailsField = (TextView)findViewById(R.id.details_field);
+        currentTemperatureField = (TextView)findViewById(R.id.current_temperature_field);
+        weatherIcon = (TextView)findViewById(R.id.weather_icon);
+
         // initialize the necessary libraries
         init();
 
@@ -167,6 +187,8 @@ public class GeoLocation extends Activity {
             );
             //show address
             txtAddressResult.setText(getAddress(mCurrentLocation));
+            //update weather fields
+            updateWeatherData(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
 
             // giving a blink animation on TextView
             txtLocationResult.setAlpha(0);
@@ -395,6 +417,59 @@ public class GeoLocation extends Activity {
             return "unknown address";
         }
 
+    }
+
+    //Get weather data
+    private void updateWeatherData(final double lat,final double lon){
+        new Thread(){
+            public void run(){
+                final JSONObject json = RemoteFetch.getJSON(getApplicationContext(), lat, lon);
+                if(json == null){
+                    handler.post(new Runnable(){
+                        public void run(){
+                            Toast.makeText(getApplicationContext(),
+                                    getString(R.string.place_not_found),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable(){
+                        public void run(){
+                            renderWeather(json);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+
+    private void renderWeather(JSONObject json){
+        try {
+            cityField.setText(json.getString("name").toUpperCase(Locale.US) +
+                    ", " +
+                    json.getJSONObject("sys").getString("country"));
+
+            JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = json.getJSONObject("main");
+            detailsField.setText(
+                    details.getString("description").toUpperCase(Locale.US) +
+                            "\n" + "Humidity: " + main.getString("humidity") + "%" +
+                            "\n" + "Pressure: " + main.getString("pressure") + " hPa");
+
+            currentTemperatureField.setText(
+                    String.format("%.2f", main.getDouble("temp"))+ " ℃");
+
+            DateFormat df = DateFormat.getDateTimeInstance();
+            String updatedOn = df.format(new Date(json.getLong("dt")*1000));
+            updatedField.setText("Last update: " + updatedOn);
+
+//            setWeatherIcon(details.getInt("id"),
+//                    json.getJSONObject("sys").getLong("sunrise") * 1000,
+//                    json.getJSONObject("sys").getLong("sunset") * 1000);
+
+        }catch(Exception e){
+            Log.e("SimpleWeather", "One or more fields not found in the JSON data");
+        }
     }
 
 }
